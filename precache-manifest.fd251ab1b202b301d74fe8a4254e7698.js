a5dca72e5ffa83e4593328ccfd8fa9e3ef3f5d86
Mahmod Jnead <63670524+MaHmOd518@users.noreply.github.com>
2022-03-29 21:32:02 +0300

    Add files via upload

---
+++ precache-manifest.fd251ab1b202b301d74fe8a4254e7698.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef45a1cd25a1449d34b3658f23e3cf87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053222034823714f5059",
+    "url": "/static/css/main.65c0ddc6.chunk.css"
+  },
+  {
+    "revision": "266b9565085a9c305753",
+    "url": "/static/js/2.79f37f0e.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/static/js/2.79f37f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053222034823714f5059",
+    "url": "/static/js/main.ad7f5352.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "0de7f8709503676ff5699e995ce2fec9",
+    "url": "/static/media/Amaranth-Bold.0de7f870.ttf"
+  },
+  {
+    "revision": "5f274104168dac1f3c25b18ffb59878f",
+    "url": "/static/media/Amaranth-Regular.5f274104.ttf"
+  }
+]);
